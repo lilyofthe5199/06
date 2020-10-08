b9dbd2fd9828f9ed359596f89479f31654e9897d
lilyofthe5199 <71003924+lilyofthe5199@users.noreply.github.com>
2020-10-08 16:05:01 +0900

    03

--- main.c
+++ main.c
@@ -3,29 +3,24 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int sumTwo(int a, int b)
+void square( int a )
 {
-	return (a+b);	
+a = a *a;
 }
 
-int square(int n)
+/*
+int squqre( int a)
 {
-	return(n*n);
+return (a*a);
 }
+*/
 
-int get_max(int x, int y)
-{		
-	if(x > y)
-		return x;
-	else
-		return y;
-}
-
-int main(int argc, char *argv[])
-{
-	ptintf("sumTwo : %i\n", sumTwo(1,2));
-	ptintf("square : %i\n", square(5));
-	ptintf("get_max : %i\n", get_max(10, 30));
-
+int main(int argc, char *argv[]) {
+	
+	int a = 2;
+	
+	square(a);
+	printf("a=%i\n", a);
+	
 	return 0;
 }
